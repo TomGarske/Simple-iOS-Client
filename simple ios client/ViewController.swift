d376d2524d2363313d82667d8b0141072c01582f
Thomas Garske <tdgarske@gmail.com>
2016-12-10 13:00:06 -0700

    added in post request for sampling rate

--- simple ios client/ViewController.swift
+++ simple ios client/ViewController.swift
@@ -11,24 +11,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let serverPath="http://192.168.0.23:5000/dev/api/motiondata"
     let motionManager = CMMotionManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(serverPath)
         recordAccelerometer()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
+// Extension for recording from accelerometer
 extension ViewController {
-
     func recordAccelerometer() {
         if motionManager.isAccelerometerAvailable {
-            let sampling = 0.05
+            let sampling = 1.0//0.05
             motionManager.accelerometerUpdateInterval = sampling
             var lastTime = TimeInterval()
             motionManager.startAccelerometerUpdates(to: OperationQueue.main) {
@@ -41,9 +37,41 @@ extension ViewController {
                 lastTime = data!.timestamp
 
                 if(time<1000){
-                    print(x,y,z,time)
+                    //print(x,y,z,time)
+                    let json = [ "x":x,"y":y,"z":z,"t":time]
+                    self.postRequest(path: self.serverPath,data:json)
                 }
             }
+        }
+    }
+}
+
+// Extension for sending post requests
+extension ViewController {
+    func postRequest(path : String, data : Dictionary<String, Double>) {
+        print(data)
+        let url = URL(string: path)!
+        let session = URLSession.shared
+
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: data, options: .prettyPrinted)
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+            request.cachePolicy = .reloadIgnoringCacheData
+            request.httpBody = jsonData
+            print(jsonData)
+
+            let task = session.dataTask(with: request){ data,response,error in
+                if error != nil{
+                    print(error!.localizedDescription)
+                    return
+                }
+                print(response!)
+            }
+            task.resume()
+        }catch {
+            print(error)
         }
     }
 }
